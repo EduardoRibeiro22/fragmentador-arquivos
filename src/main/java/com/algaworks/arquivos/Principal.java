8512231bb3d326cd86decf12beed89f22760c39d
Eduardo Ribeiro <eduardo_silva200@outlook.com>
2026-01-15 22:43:18 -0300

    Versão Final

--- src/main/java/com/algaworks/arquivos/Principal.java
+++ src/main/java/com/algaworks/arquivos/Principal.java
@@ -13,17 +13,13 @@ public class Principal {
     public static void main(String[] args) {
         try {
             logger.info("Iniciando programa de fragmentação...");
-//            System.out.println("INFO: Iniciando programa de fragmentação...");
             var fragmentador = new FragmentadorDeArquivo(
                     Path.of("fotos/picanha.jpg"),
                     1024 * 50);
             fragmentador.fragmentar();
             logger.info("Fragmentação concluída com sucesso! ");
-//            System.out.println("INFO: Fragmentação concluída com sucesso!");
         } catch (Exception e) {
             logger.error("Erro fragmentando arquivo");
-//            System.out.println("Erro fragmentando arquivo");
-            e.printStackTrace();
         }
     }
 
--- src/main/java/com/algaworks/arquivos/fragmentador/FragmentadorDeArquivo.java
+++ src/main/java/com/algaworks/arquivos/fragmentador/FragmentadorDeArquivo.java
@@ -30,23 +30,19 @@ public class FragmentadorDeArquivo {
         if (this.tamanhoFragmento < (1024 * 100)) {
 
             logger.warn("Tamanho dos fragmentos muito pequeno. Considere aumentar para no mínimo 100kb");
-//           System.out.println("WARN: Tamanho dos fragmentos muito pequeno. Considere aumentar para no mínimo 100kb");
         }
     }
 
     public void fragmentar() throws IOException {
-        logger.info("Fragmentação invocada para arquivo original" + arquivo
-                + " com fragmentos de até " + tamanhoFragmento + " bytes");
-//        System.out.println("DEBUG: Fragmentação invocada para arquivo original " + arquivo
-//                + " com fragmentos de até " + tamanhoFragmento + " bytes");
+        logger.info("Fragmentação invocada para arquivo original{} com fragmentos de até {} bytes", arquivo, tamanhoFragmento);
+
         ByteBuffer buffer = ByteBuffer.allocate(tamanhoFragmento);
         try (var canalLeitura = Files.newByteChannel(arquivo, READ)) {
             int numeroFragmento = 1;
 
             while (canalLeitura.read(buffer) > 0) {
 
-                    logger.trace("Lendo buffer de fragmento " + numeroFragmento);
-//                System.out.println("TRACE: Lendo buffer de fragmento " + numeroFragmento);
+                logger.trace("Lendo buffer de fragmento {}", numeroFragmento);
                 buffer.flip();
                 Path arquivoFragmento = criarArquivoFragmento(numeroFragmento++);
                 escreverFragmento(arquivoFragmento, buffer);
@@ -56,13 +52,12 @@ public class FragmentadorDeArquivo {
     }
 
     private Path criarArquivoFragmento(int numeroFragmento) {
-        return Path.of(arquivo.toAbsolutePath().toString() + "." + numeroFragmento);
+        return Path.of(arquivo.toAbsolutePath() + "." + numeroFragmento);
     }
 
     private void escreverFragmento(Path arquivoFragmento, ByteBuffer buffer) throws IOException {
-        logger.trace("Escrevendo fragmento em " + arquivoFragmento.getFileName());
+        logger.trace("Escrevendo fragmento em {}", arquivoFragmento.getFileName());
 
-//        System.out.println("TRACE: Escrevendo fragmento em " + arquivoFragmento.getFileName());
         try (var canalEscrita = Files.newByteChannel(arquivoFragmento, CREATE_NEW, WRITE)) {
             canalEscrita.write(buffer);
         }
